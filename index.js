8d92061cf8c2d8ed8c58df238ffbc3f98e862e9d
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-09-02 19:21:31 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(bathemydog="roller-skate"){
+return `This Saturday, I want to ${bathemydog}!`;
+}
+//function mondaywork
+function mondayWork(workfromhome="go to the office"){
+    return`This Monday, I will ${workfromhome}.`;
+}
+// depending on the symbol declared you'll get the required statement
+function wrapAdjective(symbol){
+    return function(adjective){
+        return `You are ${symbol}${adjective}${symbol}!`
+    }
+}
+    
